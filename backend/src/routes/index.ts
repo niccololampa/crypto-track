5e5075fb16375fac7f7f1cdf6e23286d34ff7ec9
Niccolo Lampa <niccololampa@gmail.com>
2024-07-04 00:29:50 +0800

    add initial price route to return sample data

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -4,8 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 const router = express.Router();
 
-router.get('/', (_req: Request, res: Response) => {
-  res.sendFile(path.resolve('public/index.html'));
+router.get('/price/:coin', (req: Request, res: Response) => {
+  const { coin } = req.params;
+  const minutes = req.query.minutes || process.env.DEFAULT_PRICE_MINUTES;
+
+  res.json({ latest: 0, average: 0, history: [], count: 0 });
 });
 
 export default router;
