42ad09ffd6befb6268ef8d8734c853a84c126c22
Niccolo Lampa <niccololampa@gmail.com>
2024-07-06 19:40:45 +0800

    update homepage to redirect to /bitcoin

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/", // The old path you want to redirect from
+        destination: "/bitcoin", // The new path you want to redirect to
+        permanent: false, // or true for a permanent redirect
+      },
+    ];
+  },
+};
 
 export default nextConfig;
